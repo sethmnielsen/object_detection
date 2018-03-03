ca4e98e4f6163dc4a53a599f063a9702a9939943
Houston <marsroverbyu@gmail.com>
2018-03-03 07:53:27 -0700

    Filtering

--- tb_detection.py
+++ tb_detection.py
@@ -30,11 +30,11 @@ from utils import visualization_utils as vis_util
 # ## Variables
 #
 # Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.
-MODEL_NAME = 'tennisball_graph4'
+MODEL_NAME = 'tennisball_graph5-2'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 # PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph_inception.pb'
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph_inception.pb'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'tb_label_map.pbtxt')
@@ -56,13 +56,14 @@ category_index = label_map_util.create_category_index(categories)
 
 # # Detection
 # Choose video feed to be a video file (e.g. 'path/to/file') or a camera input (e.g. 0 or 1)
-# cap = cv2.VideoCapture('/home/seth/Videos/vid3.mp4')
-# cap = cv2.VideoCapture('/home/seth/Videos/urc_autonomy/A4.MOV')
-cap = cv2.VideoCapture('/dev/videoZED')
-w = 640
-h = 480
+# cap = cv2.VideoCapture(os.path.expanduser('~/Videos/vid3.mp4'))
+cap = cv2.VideoCapture(os.path.expanduser('~/Videos/urc_autonomy/A4.MOV'))
+# cap = cv2.VideoCapture('/dev/videoZED')
+w = 1920
+h = 1080
 cap.set(3, w)
 cap.set(4, h)
+prev_score = 0
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
@@ -76,7 +77,7 @@ with detection_graph.as_default():
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     while cap.isOpened():
-        for i in range(2):
+        for i in range(0):
             _ = cap.grab()
         ret, frame = cap.read()
         # image_np = frame # use this line for any video feed that isn't the ZED
@@ -107,13 +108,23 @@ with detection_graph.as_default():
         y_max = corners[3]
         length = x_max-x
         height = y_max-y
+        raw_score = scores[0][0]
         squareness = 1 - abs(length-height) / (length+height)
-        if scores[0][0] > .50 and squareness > .8:
-                # Instead of fancy probability box, show simple rectangle
+        if squareness < 0:
+            raw_score = 0
+
+        alpha = 0.1
+        filt_score = (1-alpha)*prev_score + alpha*raw_score
+        prev_score = filt_score
+
+        if filt_score > .50:
+                cv2.putText(image_np,'{:2.2f}'.format(filt_score),(x, y-5),cv2.FONT_HERSHEY_SIMPLEX, 1., (0,0,255), 2)
+                cv2.rectangle(image_np, (x, y), (x_max, y_max), (0,0,255), 2)
+        elif raw_score > .50:
                 cv2.rectangle(image_np, (x, y), (x_max, y_max), (255,0,0), 2)
         else:
             corners = (-1, -1, -1, -1)
-        print str(corners) + ', {:1.2f}'.format(scores[0][0])
+        # print str(corners) + ', {:1.2f}'.format(filt_score)
         # cv2.imshow('object detection 3', cv2.resize(image_np, (640, 480)))
         cv2.imshow('object detection 2', image_np)
         if cv2.waitKey(1) & 0xFF == ord('q'):
